05cae466ec53dcba5bf1e6e3e7f91742b2e93f37
Duong Tuan with Son <son.duongtuann@gmail.com>
2026-02-11 22:36:46 +0700

    11/02/2026

--- src/main/java/vn/son/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/son/laptopshop/LaptopshopApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaptopshopApplication.class, args);
 	}
-
 }
--- src/main/java/vn/son/laptopshop/controller/UserController.java
+++ src/main/java/vn/son/laptopshop/controller/UserController.java
@@ -1,14 +1,17 @@
 package vn.son.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.son.laptopshop.domain.User;
 import vn.son.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class UserController {
@@ -19,36 +22,24 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handlHello();
+
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        String test = this.userService.handlHello();
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+
+    public String createUserPage(Model model, @ModelAttribute("newUser") User son) {
+        System.out.println("run here:" + son);
         return "hello";
     }
 
 }
-
-// @Controller
-// public class user_controller {
-
-// @RequestMapping("/")
-
-// public String getHomePage() {
-// return "hello from controller";
-// }
-// }
-
-// @RestController
-// public class UserController {
-
-// private UserService userService;
-
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-// @GetMapping("")
-// public String gethomePage() {
-// String test = this.userService.handlHello();
-// return "hello.jsp";
-// }
-
-// }
